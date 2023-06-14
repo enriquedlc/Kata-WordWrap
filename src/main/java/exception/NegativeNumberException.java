967b75190b493cf1f7ebab274b465060092091fe
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-06-14 20:58:20 +0100

    :rotating_light: test should_throw_exception_if_a_negative_number_is_given failed.

---
+++ src/main/java/exception/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class NegativeNumberException extends Exception {
+
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+
+}
--- src/test/java/WordWrapTest.java
+++ src/test/java/WordWrapTest.java
@@ -1,2 +1,22 @@
+import exception.NegativeNumberException;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class WordWrapTest {
+
+    /*
+     * :rotating_light:
+     * :white_check_mark:
+     */
+
+    public static final String HOLA = "hola";
+
+    @Test
+    public void should_throw_exception_if_a_negative_number_is_given() {
+        String exceptionMessage = "Negatives not allowed";
+        assertEquals(exceptionMessage, assertThrows(NegativeNumberException.class, () -> {
+        }).getMessage());
+    }
+
 }

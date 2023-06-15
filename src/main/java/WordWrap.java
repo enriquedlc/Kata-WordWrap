6708f8f063640df35fa2bad39122a1a4445dc5dc
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-06-15 18:27:34 +0100

    refactor:hammer: wordWrap method

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -1,25 +1,40 @@
 import exception.NegativeNumberException;
 
-import java.util.Arrays;
-
 public class WordWrap {
 
     public static final String NEW_LINE = "\n";
 
-    // TODO: refactor the method to not use recursion
-    // TODO: refactor this method to use a StringBuilder instead of String concatenation
-    // TODO: refactor conditions to be more readable
-
-    public String wordWrap(String text, int columnLength) throws NegativeNumberException {
-        if (text == null || text.isEmpty()) {
-            return "";
-        } else if (columnLength < 0) {
-            throw new NegativeNumberException("Negative column width is not valid");
-        } else if (text.length() <= columnLength) {
-            return text;
-        }
-        return text.substring(0, columnLength) + NEW_LINE + wordWrap(text.substring(columnLength), columnLength);
+    private boolean isNullOrEmpty(String text) {
+        return text == null || text.isEmpty();
     }
 
+    public String wordWrap(String text, int columnLength) throws NegativeNumberException {
+        if (isNullOrEmpty(text)) {
+            return "";
+        }
+
+        if (columnLength < 0) {
+            throw new NegativeNumberException("Negative column width is not valid");
+        }
+
+        StringBuilder wrappedText = new StringBuilder();
+        int startIndex = 0;
+
+        while (startIndex < text.length()) {
+            if (startIndex + columnLength <= text.length()) {
+                wrappedText.append(text, startIndex, startIndex + columnLength);
+                startIndex += columnLength;
+            } else {
+                wrappedText.append(text.substring(startIndex));
+                startIndex = text.length();
+            }
+
+            if (startIndex < text.length()) {
+                wrappedText.append(NEW_LINE);
+            }
+        }
+
+        return wrappedText.toString();
+    }
 
 }

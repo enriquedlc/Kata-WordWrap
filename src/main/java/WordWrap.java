f2374e5f31478da2d63ce3154e4bd756304cd94b
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-06-15 15:40:14 +0100

    :white_check_mark: test should_return_string_with_new_line_if_column_length_is_between_to_string_length passed

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -1,10 +1,21 @@
 import exception.NegativeNumberException;
 
+import java.util.Arrays;
+
 public class WordWrap {
+
+    public static final String NEW_LINE = "\n";
+
     public String wordWrap(String text, int columnLength) throws NegativeNumberException {
-        if (text == null || text.isEmpty()) return "";
-        if (columnLength < 0) throw new NegativeNumberException("Negative column width is not valid");
-        return text;
+        if (text == null || text.isEmpty()) {
+            return "";
+        } else if (columnLength < 0) {
+            throw new NegativeNumberException("Negative column width is not valid");
+        } else if (text.length() <= columnLength) {
+            return text;
+        }
+        return text.substring(0, columnLength) + NEW_LINE + wordWrap(text.substring(columnLength), columnLength);
     }
+
 
 }

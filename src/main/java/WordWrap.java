89388f218fd748292b25db03b292e445254c859c
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-06-15 15:24:22 +0100

    :white_check_mark: test should_return_empty_string_if_empty_string_is_given passed.

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -3,6 +3,7 @@ import exception.NegativeNumberException;
 public class WordWrap {
     public String wordWrap(String text, int columnLength) throws NegativeNumberException {
         if (columnLength < 0) throw new NegativeNumberException("Negative column width is not valid");
+        if (text.isEmpty()) return "";
         return text;
     }
 
